7b066df1de36fa57bb3cb2f587461c7bcf71f653
ReferencesScript <108392970+ReferencesScript@users.noreply.github.com>
2022-08-13 18:04:41 +0700

    Add files via upload

---
+++ The hats.lua
@@ -0,0 +1,18 @@
+This is the hats for the FE Gaming PC script :
+
+- https://www.roblox.com/catalog/9402627690/We-want-Peace-Sign
+(get this item from this game https://www.roblox.com/games/8528736393/Beatland and watch this tutorial on how to get the item https://www.youtube.com/watch?v=g26a5-fXM94&t=131s)
+
+- https://www.roblox.com/catalog/2309346267/Classic-PC-Hat
+(get this item from this game https://www.roblox.com/games/2546610365/Roblox-Creator-Challenge and complete all challenges by answering 3-4 questions correctly in every stage)
+
+- https://www.roblox.com/catalog/6470135113/Fan-Hand-Sign-Why-Dont-We-WDW
+
+- https://www.roblox.com/catalog/48474294/ROBLOX-Girl-Hair
+(get this bundle in order to get the hair https://www.roblox.com/bundles/282/ROBLOX-Girl)
+
+- https://www.roblox.com/catalog/62724852/Chestnut-Bun
+
+- https://www.roblox.com/catalog/451220849/Lavender-Updo
+
+- https://www.roblox.com/catalog/63690008/Pal-Hair
