8ac2817f2712f0289b895849118f262892712c06
ReferencesScript <108392970+ReferencesScript@users.noreply.github.com>
2022-08-24 21:03:24 +0700

    Update The hats.lua

--- The hats.lua
+++ The hats.lua
@@ -6,7 +6,8 @@ This is the hats for the FE Gaming PC script :
 - https://www.roblox.com/catalog/2309346267/Classic-PC-Hat
 (get this item from this game https://www.roblox.com/games/2546610365/Roblox-Creator-Challenge and complete all challenges by answering 3-4 questions correctly in every stage)
 
-- https://www.roblox.com/catalog/6470135113/Fan-Hand-Sign-Why-Dont-We-WDW
+- https://www.roblox.com/catalog/10369943613/8-Bit-Controller-Backpack
+(claim this before 1 september)
 
 - https://www.roblox.com/catalog/48474294/ROBLOX-Girl-Hair
 (get this bundle in order to get the hair https://www.roblox.com/bundles/282/ROBLOX-Girl)
